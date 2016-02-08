1da2dbbca022b648a04220d1d8659629299a8584
Albert Hermida <alberthermida@gmail.com>
2016-02-08 11:37:44 -0500

    Changed the Actions for dash, changed svg to absolute path

--- src/TeacherReview/components/SearchResults/Result.jsx
+++ src/TeacherReview/components/SearchResults/Result.jsx
@@ -2,9 +2,9 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render: function() {
-		return (<div className="search-result" type={this.props.type}> 
-			<img src="SVG/atom.svg" width="25" height="25" />
-			{this.props.name}	
+		return (<div className="search-result" type={this.props.type}>
+			<img src="http://localhost:8000/SVG/atom.svg" width="25" height="25" />
+			{this.props.name}
 		</div>);
 	}
 });
