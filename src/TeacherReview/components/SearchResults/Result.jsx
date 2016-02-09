7a50d930ece87ea145f68cd59ada35d1529b6120
david siah <dsiah@villanova.edu>
2016-02-09 18:31:32 -0500

    Cleanup and searchbar styles

--- src/TeacherReview/components/SearchResults/Result.jsx
+++ src/TeacherReview/components/SearchResults/Result.jsx
@@ -10,7 +10,7 @@ module.exports = React.createClass({
 	render: function() {
 		return (
 		<div className="search-result" type={this.props.type} onClick={this.onClick}> 
-			<img src="SVG/atom.svg" width="25" height="25" />
+			<img src="http://localhost:8000/SVG/atom.svg" width="25" height="25" />
 			{this.props.name}	
 		</div>);
 	}
